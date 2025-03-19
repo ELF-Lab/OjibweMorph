b5a18f8479acd06b0e0b30525958f3cb9afcecb9
Christopher Mathias Hammerly <cmhammerly@gmail.com>
2025-03-19 10:38:25 -0700

    Update create_fst.sh

--- create_fst.sh
+++ create_fst.sh
@@ -1,6 +1,6 @@
 # Set these variables to point to the location of these repos on your system
-PARSER_TOOLS=~/Documents/ELF/OjibweTesting/ParserTools
-OJIBWE_LEXICON=~/Documents/ELF/OjibweTesting/OjibweLexicon
+PARSER_TOOLS=~/ParserTools
+OJIBWE_LEXICON=~/OjibweLexicon
 # This path needs to be **relative to ParserTools/csv2fst**
 # For example, if both OjibweMorph and ParserTools are in your root folder,
 # set this to ../../OjibweMorph
