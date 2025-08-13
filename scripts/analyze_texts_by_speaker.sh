a49400b7af33da85609cb44dd57c36b212dcee82
Anna <annajs@student.ubc.ca>
2025-08-12 17:35:40 -0700

    Made a few minor refactoring edits to the Makefile. 	- Basically just making some shell stuff more explicit, to get things in working shape for the impending Docker support.  But should be no changes to the functionality of things.

--- scripts/analyze_texts_by_speaker.sh
+++ scripts/analyze_texts_by_speaker.sh
@@ -5,7 +5,7 @@ FOMA=$2
 for file_name in $DIR/*_example_sentences.txt; do
     if [[ "$file_name" =~ $DIR/([a-z_]+)_example_sentences.txt ]]; then
         speaker=${BASH_REMATCH[1]}
-        echo "\nAnalyzing sentences from speaker: $speaker"
+        echo "Analyzing sentences from speaker: $speaker"
         sh scripts/analyze_text.sh $file_name $FOMA
     else
         echo "Unexpected filename in $DIR: $file_name"
